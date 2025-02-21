d29adcb5686375a17063460d89554f3447878f58
brendanprogrammer <brendan.programmer@gmail.com>
2025-02-21 07:26:31 -0800

    feat: update Home page layout with Featured, CategoryList, and CardList components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,22 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
 import Footer from "@/components/footer/Footer";
-import Navbar from "@/components/navbar/Navbar";
+
 
 export default function Home() {
-  return <div>
-    <Navbar/>
-    <Footer/>
-  </div>
+  return (
+    <div className={styles.container}>
+      <Featured/>
+      <CategoryList/>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+        <Footer/>
+      </div>
+    </div>
+  );
 }

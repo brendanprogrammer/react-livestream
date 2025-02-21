a4b6eaf19a0a07544a43163dbedf3cc952b4a7b9
brendanprogrammer <brendan.programmer@gmail.com>
2025-02-21 10:42:08 -0800

    chore: develop Navbar component with social icons, navigation, and authentication integration

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <Image src="/tiktok.png" alt="tiktok" width={24} height={24}/>
                 <Image src="/youtube.png" alt="youtube" width={24} height={24}/>
             </div>
-            <div className={styles.logo}>Kingdom Connection KCee Radio</div>
+            <div className={styles.logo}>KCee Radio</div>
             <div className={styles.links}>
                 <ThemeToggle/>
                 <Link href="/">Home</Link>

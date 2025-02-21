87ed1baa493b526e036e915a15f48ab4f48991a8
brendanprogrammer <brendan.programmer@gmail.com>
2025-02-21 07:27:23 -0800

    fix: correct footer tag in RootLayout and update metadata for Kingdom Connection Radio

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
+import Navbar from "@/components/navbar/Navbar";
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
+  title: 'Kingdom Connection Radio',
+  description: "KC radio is a Christian internet radio focusing on believer's spiritual growth through worship music and faith-based materials.",
 }
 
 export default function RootLayout({ children }) {
@@ -13,7 +15,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="container">
-          <div className="wrapper">{children}</div>
+          <div className="wrapper">
+          <Navbar/>
+            {children}
+          <footer/>
+          </div>
         </div>
       </body>
     </html>
